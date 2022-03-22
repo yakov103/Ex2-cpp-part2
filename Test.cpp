3db16d97f472f69a64b604adadf23fdbfc2414dc
yakovkhodorkovski <yaakov103@gmail.com>
2022-03-22 22:21:32 +0200

    added some tests

--- Test.cpp
+++ Test.cpp
@@ -24,21 +24,31 @@ TEST_CASE("good input"){
     Notebook note ;
     note.write(100,100,50,Direction::Horizontal,"ABCD");
     CHECK(note.read(100,100,50,Direction::Horizontal,4)=="ABCD");
-    note.write(100,99,51,Direction::Vertical,"xyz");
-    CHECK(note.read(100,99,51,Direction::Vertical,3)=="xyz");
+    note.write(100,99,51,Direction::Vertical,"x");
+    note.write(100,101,51,Direction::Vertical,"z");
+    CHECK(note.read(100,99,51,Direction::Vertical,3)=="xBz");
     note.erase(100,100,51,Direction::Horizontal,2);
     CHECK(note.read(100,100,50,Direction::Horizontal,4)=="A~~D");
     CHECK(note.read(100,99,51,Direction::Vertical,3)=="x~z");
+    note.write(100,100,54,Direction::Horizontal,"EFGH");
+    CHECK(note.read(100,100,50,Direction::Horizontal,8)=="ABCDEFGH");
 
-
+    note.write(51,50,20,Direction::Horizontal,"testing");
+    CHECK(note.read(51,52,20,Direction::Horizontal,4)=="____");
+    CHECK(note.read(51,50,21,Direction::Horizontal,5)=="estin");
+    CHECK(note.read(51,50,20,Direction::Vertical,1)=="t");
 
 
 }
 
 TEST_CASE("bad input"){
 
-
-
+    Notebook note;
+    note.write(100,100,50,Direction::Horizontal,"ABCD");
+    CHECK_THROWS(note.write(100,100,50,Direction::Horizontal,"DCBA")); //need to throw because overwrite
+    CHECK_THROWS(note.write(100,99,51,Direction::Vertical,"XYZ")); // need to throw because overwrite
+    note.erase(100,100,50,Direction::Horizontal,4);
+    CHECK_THROWS(note.write(100,100,50,Direction::Horizontal,"ABCD")); // you cannot write where you delete
 
 
 }

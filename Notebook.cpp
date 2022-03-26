e7abb51674fcd4a0cba93f83061a7a5e681bd4d9
yakov103 <yaakov103@gmail.com>
2022-03-26 14:14:24 +0300

    first commit for part 2

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,12 +1,46 @@
 #include "Notebook.hpp"
+#include <map> 
+#include <unordered_map>
+
 
 
 constexpr unsigned int MAX_ROW_LEN = 100;
 
 namespace ariel {
 
-    void Notebook::write(int page, int row, int col, Direction dir, string const & str) {
 
+
+    Notebook::Notebook(){
+     unordered_map<int , unordered_map <int , string> > notebook ;  
+     this->notebook=notebook; 
+
+    }
+
+    /*
+
+    write fucntion : 
+    input : 
+    page , row , col , DIRECT , string  
+
+    if new Horizontal :
+    write the string in to lane . and add the main notebook
+
+
+    */
+    void Notebook::write(int page, int row, int col, Direction dir, string const & str) {
+        if (dir == Direction::Horizontal){
+        unordered_map <int ,string> pageOfbook ;
+        string t = "____________________________________________________________________________________________________";
+        int strLengthOfWord = str.size();
+        unsigned int colCopy = (unsigned int ) col ; 
+        for (unsigned int i = colCopy ; i < col+strLengthOfWord ; i++){ 
+              t.at(i)=str.at(i-colCopy); 
+        }
+        pageOfbook.insert(pair<int ,string>(row,t)); 
+        cout << pageOfbook.at(row) << endl; 
+       this->notebook.insert(pair<int,unordered_map<int,string> >(page,pageOfbook));
+        cout << this->notebook.at(page).at(row) << endl; 
+        }
     }
     void Notebook::erase( int page, int row, int col, Direction dir, int length) {
 
--- Notebook.hpp
+++ Notebook.hpp
@@ -2,12 +2,17 @@
 #include "Direction.hpp"
 #include <string>
 #include <stdexcept>
+#include <unordered_map>
+
 using namespace std;
 using namespace ariel;
 
+
 namespace ariel {
     class Notebook {
+       unordered_map<int , unordered_map <int , string> > notebook ;  
     public :
+        Notebook(); 
         void write(int page, int row, int col, Direction dir, string const & str);
         void erase(int page, int row, int col, Direction dir, int length);
         string read(int page, int row,  int col, Direction dir,  int length);

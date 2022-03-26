b7a245237353c156541d691ff02e9e7a30268c2f
yakovkhodorkovski <yaakov103@gmail.com>
2022-03-26 17:51:51 +0200

    matala is done

--- Notebook.cpp
+++ Notebook.cpp
@@ -5,7 +5,8 @@
 constexpr unsigned int MAX_ROW_LEN = 100;
 
 namespace ariel {
-
+     void minusValuesCheck (int const & page , int const & row , int const & col,string const & str);
+   void minusValuesCheck (int const & page , int const & row , int const & col, int const & length  );
 
 
     Notebook::Notebook(){
@@ -26,6 +27,7 @@ namespace ariel {
 
     */
     void Notebook::write(int const & page, int const & row, int const & col, Direction dir, string const & str) {
+        minusValuesCheck(page,row,col,str); 
         string t ; 
         if (this->notebook.find(page) == notebook.end()){ 
             map <int ,string> pageOfbook ;
@@ -43,7 +45,15 @@ namespace ariel {
            t = this->notebook.at(page).at(row);
 
         if (dir == Direction::Horizontal){
+            int lengthOfString = str.size(); 
+            if (MAX_ROW_LEN < col+lengthOfString){
+                throw runtime_error ("you cannot overWrite after 100 col !"); 
+            }; 
         for (unsigned int i = ((unsigned int)col) ; i < col+((int)str.size()) ; i++){ 
+            if (t.at(i) != '_'){
+                throw runtime_error("you cannot overwrite !!!"); 
+            }
+
               t.at(i)=str.at(i-((unsigned int)col)); 
         }
         this->notebook.at(page).at(row) = t ;
@@ -60,6 +70,7 @@ namespace ariel {
     
     }
     void Notebook::erase( int page, int row, int col, Direction dir, int length) {
+        minusValuesCheck(page,row,col,length); 
         string t ; 
         if (this->notebook.find(page) == notebook.end()){ 
             map <int ,string> pageOfbook ;
@@ -92,6 +103,7 @@ namespace ariel {
 
     }
     string Notebook::read(int page, int row, int col, Direction dir, int length) {
+    minusValuesCheck(page,row,col,length);
      string resulat; 
     if ( this->notebook.find(page) != notebook.end()){
         if (dir == Direction::Horizontal ){ 
@@ -126,11 +138,31 @@ namespace ariel {
     }
 
     void Notebook::show(int page) {
+        if ( page < 0 ){
+        throw runtime_error("no negative page !"); 
+        }
         map <int ,string> rows = notebook.at(page); 
         for(auto const & row : rows){
         cout << row.second << endl ; 
         }
     }
+
+    void minusValuesCheck (int const & page , int const & row , int const & col,string const & str ){
+    if (page < 0 || row < 0 || col < 0 ){ 
+        throw runtime_error ( "no nagetive values on bored !!"); 
+    }
+    
+
+    } 
+    void minusValuesCheck (int const & page , int const & row , int const & col, int const & length  ){
+         if (page < 0 || row < 0 || col < 0 || length < 0  ){ 
+        throw runtime_error ( "no nagetive values on bored !!"); 
+    }
+
+        if (MAX_ROW_LEN < col+length ){
+            throw runtime_error("cannot overflow the notebook ! "); 
+        }
+    } 
 
 
 
--- Test.cpp
+++ Test.cpp
@@ -47,27 +47,29 @@ TEST_CASE("bad input"){
     note.write(100,100,50,Direction::Horizontal,"ABCD");
     CHECK_THROWS(note.write(100,100,50,Direction::Horizontal,"DCBA")); //need to throw because overwrite
     CHECK_THROWS(note.write(100,99,51,Direction::Vertical,"XYZ")); // need to throw because overwrite
-    // note.erase(100,100,50,Direction::Horizontal,4);
-    // CHECK_THROWS(note.write(100,100,50,Direction::Horizontal,"ABCD")); // you cannot write where you delete
-    // note.write(50,100,50,Direction::Horizontal,"yakov");
-    // note.write(51,100,50,Direction::Vertical,"try");
-    // CHECK_FALSE(note.read(50,100,50,Direction::Horizontal,5) == "yaoov");
-    // CHECK_FALSE(note.read(51,100,50,Direction::Vertical,3) == "tri");
-    // CHECK_THROWS(note.write(-100,100,50,Direction::Horizontal,"ABCD")); // no minus values
-    // CHECK_THROWS(note.write(100,-100,50,Direction::Horizontal,"abcd")); // no minus values
-    // CHECK_THROWS(note.write(100,100,-50,Direction::Horizontal,"rfvt")); // no minus values
-    // CHECK_THROWS(note.read(100,100,-50,Direction::Horizontal,5)); // no minus values
-    // CHECK_THROWS(note.read(100,100,50,Direction::Horizontal,-5)); // no minus values
-    // CHECK_THROWS(note.erase(100,-100,50,Direction::Horizontal,5)); // no minus values
-    // CHECK_THROWS(note.erase(100,100,50,Direction::Horizontal,-5)); // no minus values
-    // CHECK_THROWS (note.show(-1)); // no minus values 
+    note.erase(100,100,50,Direction::Horizontal,4);
+    CHECK_THROWS(note.write(100,100,50,Direction::Horizontal,"ABCD")); // you cannot write where you delete
+    note.write(50,100,50,Direction::Horizontal,"yakov");
+    note.write(51,100,50,Direction::Vertical,"try");
+    CHECK_FALSE(note.read(50,100,50,Direction::Horizontal,5) == "yaoov");
+    CHECK_FALSE(note.read(51,100,50,Direction::Vertical,3) == "tri");
+     CHECK_THROWS(note.write(-100,100,50,Direction::Horizontal,"ABCD")); // no minus values
+     CHECK_THROWS(note.write(100,-100,50,Direction::Horizontal,"abcd")); // no minus values
+     CHECK_THROWS(note.write(100,100,-50,Direction::Horizontal,"rfvt")); // no minus values
+    CHECK_THROWS(note.read(100,100,-50,Direction::Horizontal,5)); // no minus values
+    CHECK_THROWS(note.read(100,100,50,Direction::Horizontal,-5)); // no minus values
+    CHECK_THROWS(note.erase(100,-100,50,Direction::Horizontal,5)); // no minus values
+    CHECK_THROWS(note.erase(100,100,50,Direction::Horizontal,-5)); // no minus values
+    CHECK_THROWS (note.show(-1)); // no minus values 
 
-    // CHECK_THROWS(note.read(100,100,99,Direction::Horizontal,5));// cannot do over 100 col
-    // CHECK_THROWS(note.erase(100,100,99,Direction::Horizontal,5));// cannot do over 100 col 
-    // CHECK_THROWS(note.read(50,100,0,Direction::Horizontal,101));// cannot do over 100 col  
+    CHECK_THROWS(note.write(100,100,99,Direction::Horizontal,"yakov_tester"));// cannot do over 100 col
+    CHECK_THROWS(note.read(100,100,99,Direction::Horizontal,5));// cannot do over 100 col
+    CHECK_THROWS(note.erase(100,100,99,Direction::Horizontal,5));// cannot do over 100 col 
+    CHECK_THROWS(note.read(50,100,0,Direction::Horizontal,101));// cannot do over 100 col  
+    
 
-    // CHECK_THROWS(note.read(-50,100,0,Direction::Horizontal,101));// mix case  
-    // CHECK_THROWS(note.read(50,-100,99,Direction::Horizontal,3));// mix case  
+    CHECK_THROWS(note.read(-50,100,0,Direction::Horizontal,101));// mix case  
+    CHECK_THROWS(note.read(50,-100,99,Direction::Horizontal,3));// mix case  
 
 
 
